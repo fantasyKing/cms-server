00fcaad53ab7f529e9dccbf6cd5226d0250c36af
zhangGuojin <zhanggjjim@163.com>
2016-11-23 11:26:06 +0800

    增加了user list

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # get 'user/index'
+  # get 'home/index'
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
@@ -8,6 +9,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'home#index'
+  resources :users, only: [:show]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
