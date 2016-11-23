4fc21802c0884553ae4bff5a7cab1d18563b18a6
zhangGuojin <zhanggjjim@163.com>
2016-11-23 12:41:45 +0800

    user model中增加了is_admin字段

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,6 @@ class User
   field :last_sign_in_at,    type: Time
   field :current_sign_in_ip, type: String
   field :last_sign_in_ip,    type: String
-
   ## Confirmable
   # field :confirmation_token,   type: String
   # field :confirmed_at,         type: Time
@@ -33,4 +32,5 @@ class User
   # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
   # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
   # field :locked_at,       type: Time
+  field :is_admin, type: Boolean, default: false
 end
