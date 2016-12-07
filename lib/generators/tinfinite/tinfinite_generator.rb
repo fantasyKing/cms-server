fc4bdaa7156bfbbe4aae83e64072e74acb1f5901
zhangGuojin <zhanggjjim@163.com>
2016-12-07 16:13:37 +0800

    增加了view generatot

--- lib/generators/tinfinite/tinfinite_generator.rb
+++ lib/generators/tinfinite/tinfinite_generator.rb
@@ -35,15 +35,24 @@ class TinfiniteGenerator < Rails::Generators::NamedBase
     StringCases.snake_to_camel(file_name)
   end
 
-  def create_controller_file
-    template "controller/controller_template.erb", "app/controllers/db_#{db_name}/#{model_name.pluralize}_controller.rb"
-  end
+  # def create_controller_file
+  #   template "controller/controller_template.erb", "app/controllers/db_#{db_name}/#{model_name.pluralize}_controller.rb"
+  # end
 
-  def create_grid_file
-    template "grid/grid_template.erb", "app/grids/#{file_name}_grid.rb"
-  end
+  # def create_grid_file
+  #   template "grid/grid_template.erb", "app/grids/#{file_name}_grid.rb"
+  # end
 
-  def create_model_file
-    template "model/model_template.erb", "app/models/#{file_name}.rb"
+  # def create_model_file
+  #   template "model/model_template.erb", "app/models/#{file_name}.rb"
+  # end
+
+  def create_view_file
+    template "view/_actions.html.erb", "app/views/db_#{db_name}/#{model_name.pluralize}/_actions.html.erb"
+    template "view/_form.html.erb", "app/views/db_#{db_name}/#{model_name.pluralize}/_form.html.erb"
+    template "view/edit.html.erb", "app/views/db_#{db_name}/#{model_name.pluralize}/edit.html.erb"
+    template "view/index.html.erb", "app/views/db_#{db_name}/#{model_name.pluralize}/index.html.erb"
+    template "view/new.html.erb", "app/views/db_#{db_name}/#{model_name.pluralize}/new.html.erb"
+    template "view/show.html.erb", "app/views/db_#{db_name}/#{model_name.pluralize}/show.html.erb"
   end
 end
